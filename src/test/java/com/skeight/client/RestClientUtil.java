2ae4926f08d0f40524e413effc3df204f1dcb34d
Miquel Gustran <mgustran@hiberus.com>
2017-06-12 13:54:02 +0200

    casos Test

--- src/test/java/com/skeight/client/RestClientUtil.java
+++ src/test/java/com/skeight/client/RestClientUtil.java
@@ -2,32 +2,39 @@ package com.skeight.client;
 
 import java.net.URI;
 
+import com.skeight.entity.Spot;
+import org.junit.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
 import com.skeight.entity.Article;
 
+@CrossOrigin(origins = "http://localhost:8082")
+
 public class RestClientUtil {
+    @Test
     public void getArticleByIdDemo() {
     	HttpHeaders headers = new HttpHeaders();
     	headers.setContentType(MediaType.APPLICATION_JSON);
         RestTemplate restTemplate = new RestTemplate();
-	    String url = "http://localhost:8080/user/article/{id}";
+	    String url = "http://localhost:8080/data/article/{id}";
         HttpEntity<String> requestEntity = new HttpEntity<String>(headers);
-        ResponseEntity<Article> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, Article.class, 1);
+        ResponseEntity<Article> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, Article.class, 22);
         Article article = responseEntity.getBody();
         System.out.println("Id:"+article.getArticleId()+", Title:"+article.getModel()
                  +", Category:"+article.getCategory());      
     }
+    @Test
 	public void getAllArticlesDemo() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
         RestTemplate restTemplate = new RestTemplate();
-	    String url = "http://localhost:8080/user/articles";
+	    String url = "http://localhost:8080/data/articles";
         HttpEntity<String> requestEntity = new HttpEntity<String>(headers);
         ResponseEntity<Article[]> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, Article[].class);
         Article[] articles = responseEntity.getBody();
@@ -36,30 +43,37 @@ public class RestClientUtil {
                       +", Category: "+article.getCategory());
         }
     }
-    public void addArticleDemo() {
+    @Test
+    public void addSpotDemo() {
     	HttpHeaders headers = new HttpHeaders();
     	headers.setContentType(MediaType.APPLICATION_JSON);
         RestTemplate restTemplate = new RestTemplate();
-	    String url = "http://localhost:8080/user/article";
-	    Article objArticle = new Article();
-	    objArticle.setModel("Spring REST Security using Hibernate");
-	    objArticle.setCategory("Spring");
-        HttpEntity<Article> requestEntity = new HttpEntity<Article>(objArticle, headers);
+	    String url = "http://localhost:8080/spots/spot";
+	    Spot spot = new Spot();
+	    spot.setName("SkateABC");
+	    spot.setAddress("Calle Almost 123");
+        spot.setLocation("Palma de Mallorca");
+        spot.setGmapsLocation1(2.99654987);
+	    spot.setGmapsLocation2(3.9876548);
+	    spot.setImg("../img/articles/tablas/almost/3.jpg");
+        HttpEntity<Spot> requestEntity = new HttpEntity<Spot>(spot, headers);
         URI uri = restTemplate.postForLocation(url, requestEntity);
         System.out.println(uri.getPath());    	
     }
-    public void updateArticleDemo() {
+    @Test
+    public void updateSpotDemo() {
     	HttpHeaders headers = new HttpHeaders();
     	headers.setContentType(MediaType.APPLICATION_JSON);
         RestTemplate restTemplate = new RestTemplate();
-	    String url = "http://localhost:8080/user/article";
-	    Article objArticle = new Article();
-	    objArticle.setArticleId(1);
-	    objArticle.setModel("Update:Java Concurrency");
-	    objArticle.setCategory("Java");
-        HttpEntity<Article> requestEntity = new HttpEntity<Article>(objArticle, headers);
+	    String url = "http://localhost:8080/spots/spot";
+	    Spot spot = new Spot();
+	    spot.setSpotId(4);
+	    spot.setName("Chiringuito Playero");
+	    spot.setLocation("A Saber");
+        HttpEntity<Spot> requestEntity = new HttpEntity<Spot>(spot, headers);
         restTemplate.put(url, requestEntity);
     }
+    @Test
     public void deleteArticleDemo() {
     	HttpHeaders headers = new HttpHeaders();
     	headers.setContentType(MediaType.APPLICATION_JSON);
@@ -70,9 +84,9 @@ public class RestClientUtil {
     }
     public static void main(String args[]) {
     	RestClientUtil util = new RestClientUtil();
-        //util.getArticleByIdDemo();
+        util.getArticleByIdDemo();
     	util.getAllArticlesDemo();
-    	//util.addArticleDemo();
+    	util.addSpotDemo();
     	//util.updateArticleDemo();
     	//util.deleteArticleDemo();
     }    
